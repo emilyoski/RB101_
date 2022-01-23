f2869d4903c92e4243945a0e8cf4b8dfa27a2912
Emily Olszewski <eolszewski14@gmail.com>
2022-01-23 16:52:21 -0500

    Updating Mortgage_calc assignment and adding RPS Bonus

--- lesson2/mortgage_calc.rb
+++ lesson2/mortgage_calc.rb
@@ -1,7 +1,16 @@
 # building a mortgage calculator
+system 'clear'
 
 def prompt(words)
   puts "-> #{words}"
+end
+
+def valid_input?(num)
+  if num >= 0
+    "yes"
+  else
+    "no"
+  end
 end
 
 prompt("Welcome to the Mortgage Calculator!")
@@ -14,23 +23,37 @@ loop do
   prompt("Please enter your loan amount in dollars.")
   loan_amount = gets.chomp.to_i
 
+  break if valid_input?(loan_amount) == 'yes'
+  prompt("Invalid input. Please enter your information again.")
+end
+
+loop do
   prompt("Please enter your Annual Percentage Rate (APR) in %.")
   annual_rate = gets.chomp.to_f
 
+  break if valid_input?(annual_rate) == 'yes'
+  prompt("Invalid input. Please enter your information again.")
+end
+
+loop do
   prompt("Please enter your loan duration in years.")
   loan_dur_years = gets.chomp.to_i
 
-  break if (loan_amount > 0) && (annual_rate > 0) && (loan_dur_years > 0)
-
+  break if valid_input?(loan_dur_years) == 'yes'
   prompt("Invalid input. Please enter your information again.")
 end
 
 month_rate_display = annual_rate / 12
 month_rate_use = (annual_rate / 12) * 0.01
 loan_dur_month = loan_dur_years * 12
-month_payment = loan_amount *
-                (month_rate_use / (1 - (1 + month_rate_use)**(-loan_dur_month)))
+
+if month_rate_use > 0
+  month_payment = loan_amount *
+                 (month_rate_use / (1 - (1 + month_rate_use)**(-loan_dur_month)))
+else
+  month_payment = loan_amount / loan_dur_month
+end
 
 prompt("Your monthly interest rate is: #{month_rate_display}%.")
 prompt("Your loan duration in months is: #{loan_dur_month} months.")
-prompt("Your monthly payment is: $#{month_payment}.")
+prompt("Your monthly payment is: $#{format('%.2f', month_payment)}.")
---
+++ lesson2/rock_paper_scissors.rb
@@ -0,0 +1,88 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+
+winning_combos = { rock: ['lizard', 'scissors'],
+                   paper: ['rock', 'spock'],
+                   scissors: ['paper', 'lizard'],
+                   spock: ['scissors', 'rock'],
+                   lizard: ['paper', 'spock'] }
+
+def win?(first, second, winning_methods)
+  answer = first.to_sym
+  winning_results = winning_methods[answer]
+  winning_results.include?(second)
+end
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def display_results(player, computer, win_methods)
+  if win?(player, computer, win_methods)
+    puts "You won!"
+  elsif win?(computer, player, win_methods)
+    puts "Computer won!"
+  else
+    puts "It's a tie!"
+  end
+end
+
+def determine_winner(play_wins, comp_wins)
+  if play_wins == 3
+    prompt("You are the MASTER of the game!")
+  elsif comp_wins == 3
+    prompt("The computer is the MASTER of the game!")
+  end
+end
+
+player_wins = 0
+computer_wins = 0
+
+loop do
+  choice = ' '
+  loop do
+    prompt("Enter the first letter of your choice: #{VALID_CHOICES.join(', ')}")
+    choice_letter = Kernel.gets().chomp()
+
+    if choice_letter == 's'
+      prompt("Did you mean scissors or spock?")
+      choice = Kernel.gets().chomp()
+    elsif choice_letter == 'r'
+      choice = 'rock'
+    elsif choice_letter == 'p'
+      choice = 'paper'
+    elsif choice_letter == 'l'
+      choice = 'lizard'
+    end
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
+
+  display_results(choice, computer_choice, winning_combos)
+
+  player_won = false
+  computer_won = false
+
+  player_won = true if win?(choice, computer_choice, winning_combos)
+  computer_won = true if win?(computer_choice, choice, winning_combos)
+
+  if player_won
+    player_wins += 1
+  elsif computer_won
+    computer_wins += 1
+  end
+
+  determine_winner(player_wins, computer_wins)
+
+  break if player_wins == 3
+  break if computer_wins == 3
+end
+
+prompt("Thank you for playing, Good bye!")
